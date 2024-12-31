a9d3477294f2ee1c1aba4d4744e3d47b456d4af8
gtla6224422 <nonop01@163.com>
2024-12-31 12:02:41 +0800

    增加监控埋点、中间件方法

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,30 @@
 # app/__init__.py
 
-from flask import Flask
+from flask import Flask, request
 from .conf.config import Config
 from .model.models import db
 from .views_login import login_bp,bcrypt # 导入蓝图
 from .views_order import Order_bp # 导入蓝图
 from .views_tools import Tools_bp
 from flask_migrate import migrate
+from prometheus_client import Counter, Histogram, start_http_server
 import redis
+import time
 
 app = None
+
+    # 定义计数器和直方图，用于grafana采集接口性能
+REQUEST_COUNT = Counter('flask_request_count', 'App Request Count', ['method', 'endpoint', 'http_status'])
+REQUEST_LATENCY = Histogram('flask_request_latency_seconds', 'Request latency', ['method', 'endpoint'])
+    # 中间件函数
+def before_request():
+    request.start_time = time.time()
+
+def after_request(response):
+    request_latency = time.time() - request.start_time
+    REQUEST_LATENCY.labels(request.method, request.path).observe(request_latency)
+    REQUEST_COUNT.labels(request.method, request.path, response.status_code).inc()
+    return response
 
 def create_app():
     app = Flask(__name__)
@@ -27,6 +42,10 @@ def create_app():
     
     # 初始化 Redis
     app.redis = redis.StrictRedis(host='1.14.155.39', port=6379, db=0,password='lo633533')
+
+    # 注册中间件
+    app.before_request(before_request)
+    app.after_request(after_request)
 
     # 注册蓝图
     with app.app_context():
--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 # coding=utf-8
 
 from app import create_app
+from prometheus_client import Counter, Histogram, start_http_server
+from flask import Flask, request
+import time
 import os
 import sys
 
@@ -10,4 +13,5 @@ app = create_app()
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 if __name__ == '__main__':
+    start_http_server(5002)
     app.run(host='0.0.0.0', port=5001, debug=True)  
